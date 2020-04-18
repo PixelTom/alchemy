4f4e98271206b247fbd3e39a8352a9c50efe7bd4
Tom Gattenhof <tom@pixeltom.net>
2020-04-18 20:59:48 +1000

    Try a password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :password_protect
+
+  private
+
+  def password_protect
+    return unless ENV['PASSWORD'].present?
+    authenticate_or_request_with_http_basic("My Site, not yours") do |password|
+      password == ENV['PASSWORD']
+    end
+  end
+
+ end
