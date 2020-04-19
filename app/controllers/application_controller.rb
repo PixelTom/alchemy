f06be236948e82a0fe9523e26ca3f15e2a0e318c
Tom Gattenhof <tom@pixeltom.net>
2020-04-19 11:50:20 +1000

    Comment out the thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :password_protect
+end
 
-  private
+# class ApplicationController < ActionController::Base
+#     before_action :password_protect
 
-  def password_protect
-    return unless ENV['PASSWORD'].present?
-    authenticate_or_request_with_http_basic("My Site, not yours") do |password|
-      password == ENV['PASSWORD']
-    end
-  end
+#   private
 
- end
+#   def password_protect
+#     return unless ENV['PASSWORD'].present?
+#     authenticate_or_request_with_http_basic("My Site, not yours") do |password|
+#       password == ENV['PASSWORD']
+#     end
+#   end
+
+#  end
